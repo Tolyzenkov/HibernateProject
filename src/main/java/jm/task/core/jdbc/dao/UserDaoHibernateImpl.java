7c833c5db5d0ca2db26568261b043cdc729ea6f9
denis <tolyzenkov.dn@gmail.com>
2021-05-31 11:19:16 +1000

    Итог

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -4,6 +4,7 @@ import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.util.Util;
 import org.hibernate.Session;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoHibernateImpl extends Util implements UserDao {
@@ -21,59 +22,78 @@ public class UserDaoHibernateImpl extends Util implements UserDao {
                 "lastName VARCHAR(20) NOT NULL, " +
                 "age INT NOT NULL);";
 
+
         Session session = getSessionFactory().openSession();
         session.beginTransaction();
         session.createSQLQuery(sql).executeUpdate();
         session.getTransaction().commit();
         session.close();
+        System.out.println("Таблица создана");
+
 
     }
 
     @Override
     public void dropUsersTable() {
+
         Session session = getSessionFactory().openSession();
         session.beginTransaction();
-        session.createSQLQuery("DROP TABLE IF EXISTS user").executeUpdate();
+        session.createSQLQuery("DROP TABLE IF EXISTS users").executeUpdate();
         session.getTransaction().commit();
         session.close();
+        System.out.println("Таблица удалена");
+
+
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
         User user = new User(name, lastName, age);
+
         Session session = getSessionFactory().openSession();
         session.beginTransaction();
         session.save(user);
         session.getTransaction().commit();
         session.close();
-        System.out.println("Пользователь добавлен");
+        System.out.println("Пользователь с имененм " + name + " добавлен");
+
     }
 
     @Override
     public void removeUserById(long id) {
+
         Session session = getSessionFactory().openSession();
         session.beginTransaction();
         session.delete(session.get(User.class, id));
         session.getTransaction().commit();
         session.close();
+        System.out.println("Пользователь удален");
+
     }
 
     @Override
     public List<User> getAllUsers() {
-        List<User> result;
+        List<User> result = new ArrayList<>();
+        String sql = "from User";
+
         Session session = getSessionFactory().openSession();
         session.beginTransaction();
+        result = session.createQuery(sql).list();
         session.getTransaction().commit();
         session.close();
-        return session.createSQLQuery("from user").list();
+
+        return result;
     }
 
     @Override
     public void cleanUsersTable() {
+
         Session session = getSessionFactory().openSession();
         session.beginTransaction();
-        session.createSQLQuery("delete from user").executeUpdate();
+        session.createSQLQuery("delete from users").executeUpdate();
         session.getTransaction().commit();
         session.close();
+        System.out.println("Таблица очищена");
+
     }
 }
